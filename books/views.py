12f36d4ea1cf8232a273afe0ef20558e1091b38a
Jim Lamb <james.lamb.ctr@ncis.navy.mil>
2018-01-19 11:27:18 -0500

    Add ModelForm for creating new book review requests

--- books/views.py
+++ books/views.py
@@ -1,27 +1,29 @@
+from django.core.urlresolvers import reverse
 from django.db.models import Count
-from django.shortcuts import get_object_or_404,render,redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import DetailView, View
+from django.views.generic.edit import CreateView
+from .forms import BookForm, ReviewForm
 from .models import Author, Book
-from .forms import ReviewForm
+
 
 # Create your views here.
 def list_books(request):
     """
     List the books that have reviews
-    :param request:
-    :return:
     """
+
     books = Book.objects.exclude(date_reviewed__isnull=True).prefetch_related('authors')
 
     context = {
         'books': books,
     }
 
-    return render(request, 'list.html', context)
+    return render(request, "list.html", context)
+
 
 class AuthorList(View):
     def get(self, request):
-
         authors = Author.objects.annotate(
             published_books=Count('books')
         ).filter(
@@ -30,31 +32,50 @@ class AuthorList(View):
 
         context = {
             'authors': authors,
-
         }
 
-        return render(request, 'authors.html', context)
+        return render(request, "authors.html", context)
+
 
 class BookDetail(DetailView):
     model = Book
-    template_name = 'book.html'
+    template_name = "book.html"
+
 
 class AuthorDetail(DetailView):
     model = Author
-    template_name = 'author.html'
+    template_name = "author.html"
 
 
-def review_books(request):
+class ReviewList(View):
     """
     List all of the books that we want to review.
     """
-    books = Book.objects.filter(date_reviewed__isnull=True).prefetch_related('authors')
-    form = ReviewForm
-    context = {
-        'books': books,
-    }
 
-    return render(request, "list-to-review.html", context)
+    def get(self, request):
+        books = Book.objects.filter(date_reviewed__isnull=True).prefetch_related('authors')
+
+        context = {
+            'books': books,
+            'form': BookForm,
+        }
+
+        return render(request, "list-to-review.html", context)
+
+    def post(self, request):
+        form = BookForm(request.POST)
+        books = Book.objects.filter(date_reviewed__isnull=True).prefetch_related('authors')
+
+        if form.is_valid():
+            form.save()
+            return redirect('review-books')
+
+        context = {
+            'form': form,
+            'books': books,
+        }
+
+        return render(request, "list-to-review.html", context)
 
 
 def review_book(request, pk):
@@ -62,24 +83,33 @@ def review_book(request, pk):
     Review an individual book
     """
     book = get_object_or_404(Book, pk=pk)
+
     if request.method == 'POST':
         # Process our form
         form = ReviewForm(request.POST)
 
-
         if form.is_valid():
-            book.is_favorite = form.cleaned_data['is_favorite']
+            book.is_favourite = form.cleaned_data['is_favourite']
             book.review = form.cleaned_data['review']
             book.save()
 
             return redirect('review-books')
+
     else:
         form = ReviewForm
 
-    form = ReviewForm
     context = {
         'book': book,
         'form': form,
     }
 
     return render(request, "review-book.html", context)
+
+
+class CreateAuthor(CreateView):
+    model = Author
+    fields = ['name', ]
+    template_name = "create-author.html"
+
+    def get_success_url(self):
+        return reverse('review-books')
