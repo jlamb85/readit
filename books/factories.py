0469dd5d3966c8dee5cbe07052bcc398ba42ef9e
Jim Lamb <james.lamb.ctr@ncis.navy.mil>
2018-01-22 08:43:20 -0500

    Add unit tests for Book model

--- books/factories.py
+++ books/factories.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.utils.timezone import now
 from .models import Author, Book
 
-class AuthorFactrory(factory.django.DjangoModelFactory):
+class AuthorFactory(factory.django.DjangoModelFactory):
     """
     Creates an author.
     """
